3e007667d8409e186e5d38582b79a33fd36f1f53
sergeikozlov <siarhei.k@bluetape.com>
2024-09-12 14:59:56 +0300

    yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
 
     use: {
         // Base URL to use in actions like `await page.goto('/')`.
-        baseURL: 'http://host.docker.internal:8000',
+        baseURL: 'http://127.0.0.1:8000',
 
         // Collect trace when retrying the failed test.
         trace: 'on-first-retry',
